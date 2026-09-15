45f7bedd7b74e17a2412900df896d0986a00a109
Gordon Bie <gordonbie123@gmail.com>
2026-09-15 13:19:46 -0400

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.val = value
+        self.next = None
+        self.prev = None
+    
+class LRUCache:
+    def __init__(self, capacity):
+        self.cap = capacity
+        self.map = {}
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+    
+    def _remove(self, node):
+        node.next.prev = node.prev
+        node.prev.next = node.next
+    
+    def _add(self, node):
+        p = self.tail.prev
+        p.next = node
+        node.prev = p
+        node.next = self.tail
+        self.tail.prev = node
+    
+    def get(self, key):
+        if key not in self.map:
+            return -1
+        node = self.map[key]
+        self._remove(node)
+        self._add(node)
+        return node.val
+    
+    def put(self, key, value):
+        if key in self.map:
+            self._remove(self.map[key])
+        node = Node(key, value)
+        self.map[key] = node
+        self._add(node)
+        if len(self.map) > self.cap:
+            lru = self.head.next
+            self._remove(lru)
+            del self.map[lru.key]
+            
