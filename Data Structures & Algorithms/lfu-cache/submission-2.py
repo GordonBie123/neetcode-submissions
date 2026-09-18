821b4b532faff7911ff3cca46a43ba5cd4a14fc2
Gordon Bie <gordonbie123@gmail.com>
2026-09-17 23:21:29 -0400

    Add: lfu-cache - submission-2

---
+++ Data Structures & Algorithms/lfu-cache/submission-2.py
@@ -0,0 +1,51 @@
+class LFUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.val = {}
+        self.freq = {}
+        self.buckets = defaultdict(OrderedDict)
+        self.min_freq = 0
+    
+    def _bump(self, key):
+        frequency = self.freq[key]
+        del self.buckets[frequency][key]
+        if not self.buckets[frequency]:
+            del self.buckets[frequency]
+            if self.min_freq == frequency:
+                self.min_freq += 1
+        self.freq[key] = frequency + 1
+        self.buckets.setdefault(frequency + 1, {})[key] = None
+        
+    def get(self, key: int) -> int:
+        if key not in self.val:
+            return -1
+        self._bump(key)
+        return self.val[key]
+        
+    def put(self, key: int, value: int) -> None:
+        if self.cap <= 0:
+            return
+        if key in self.val:
+            self.val[key] = value
+            self._bump(key)
+            return
+        if len(self.val) >= self.cap:
+            lfbucket = self.buckets[self.min_freq]
+            evict = next(iter(lfbucket))
+            del lfbucket[evict]
+            if not lfbucket:
+                del self.buckets[self.min_freq]
+            del self.val[evict]
+            del self.freq[evict]
+        self.val[key] = value
+        self.freq[key] = 1
+        self.buckets.setdefault(1, {})[key] = None
+        self.min_freq = 1
+        
+
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
